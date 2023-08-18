d69538dbfb9a702db54a582dc7881995e2d4a233
Jonathan Norestin <jnorestin@gmail.com>
2023-08-18 18:14:56 +0000

    update

--- src/app.py
+++ src/app.py
@@ -8,141 +8,216 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User, People, Planet, Favorites
+from models import db, People, Planet, User, Favorites
+import json
 
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-
-db_url = os.getenv("DATABASE_URL")
-if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
-else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
 
+# Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
+# generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
-    return jsonify(response_body), 200
 
-# User Endpoints
-@app.route('/users', methods=['GET'])
-def list_users():
-    users = User.query.all()
-    return jsonify([user.serialize() for user in users])
 
-@app.route('/users/<int:user_id>', methods=['GET'])
-def get_user(user_id):
-    user = User.query.get(user_id)
-    if not user:
-        return jsonify({"error": "User not found"}), 404
-    return jsonify(user.serialize())
 
-# People Endpoints
+
 @app.route('/people', methods=['GET'])
-def get_people():
-    people = People.query.all()
-    return jsonify([person.name for person in people])
+def get_all_people():
+
+    people = People.query.all() 
+    total_people = list(map(lambda item: item.serialize(), people))
+
+    return jsonify(total_people), 200
+
+#planet endpoints 
+
+@app.route('/planets', methods=['GET'])
+def get_all_planets():
+
+    planets = Planet.query.all() 
+    total_planets = list(map(lambda item: item.serialize(), planets))
+
+    return jsonify(total_planets), 200
+
+
 
 @app.route('/people/<int:people_id>', methods=['GET'])
 def get_person(people_id):
-    person = People.query.get(people_id)
-    if person:
-        return jsonify(person.name)
-    return jsonify({"error": "Not Found"}), 404
 
-# Planets Endpoints
-@app.route('/planets', methods=['GET'])
-def get_planets():
-    planets = Planet.query.all()
-    return jsonify([planet.name for planet in planets])
+    person = People.query.filter_by(id=people_id).first()
 
-@app.route('/planets/<int:planet_id>', methods=['GET'])
-def get_planet(planet_id):
-    planet = Planet.query.get(planet_id)
-    if planet:
-        return jsonify(planet.name)
-    return jsonify({"error": "Not Found"}), 404
-
-# Add a new favorite person to the current user
-@app.route('/favorite/people/<int:people_id>', methods=['POST'])
-def add_favorite_people(people_id):
-    person = People.query.get(people_id)
-    if not person:
-        return jsonify({"error": "Person not found"}), 404
-
-    # Using first user as a placeholder
-    user = User.query.first()
-    if not user:
-        return jsonify({"error": "User not found"}), 404
-
-    # Check if this favorite already exists
-    existing_favorite = db.session.query(Favorites).filter_by(
-        user_id=user.id, entity_id=people_id, entity_type="person"
-    ).first()
-    if existing_favorite:
-        return jsonify({"error": "Favorite already exists"}), 400
-
-    # Add to favorites
-    new_favorite = {"user_id": user.id, "entity_id": people_id, "entity_type": "person"}
-    db.session.add(Favorites, new_favorite)
-    db.session.commit()
-    return jsonify({"success": "Favorite added"}), 200
-
-# Delete favorite planet for current user
-@app.route('/favorite/planet/<int:planet_id>', methods=['DELETE'])
-def delete_favorite_planet(planet_id):
-    # Using first user as a placeholder
-    user = User.query.first()
-    if not user:
-        return jsonify({"error": "User not found"}), 404
-
-    favorite = db.session.query(Favorites).filter_by(
-        user_id=user.id, entity_id=planet_id, entity_type="planet"
-    ).first()
-    if not favorite:
-        return jsonify({"error": "Favorite not found"}), 404
-
-    db.session.delete(favorite)
-    db.session.commit()
-    return jsonify({"success": "Favorite deleted"}), 200
+    return jsonify(person.serialize()), 200
 
 
-# Delete favorite person for current user
-@app.route('/favorite/people/<int:people_id>', methods=['DELETE'])
-def delete_favorite_people(people_id):
-    # Using first user as a placeholder
-    user = User.query.first()
-    if not user:
-        return jsonify({"error": "User not found"}), 404
 
-    favorite = db.session.query(Favorites).filter_by(
-        user_id=user.id, entity_id=people_id, entity_type="person"
-    ).first()
-    if not favorite:
-        return jsonify({"error": "Favorite not found"}), 404
+@app.route('/planets/<int:planets_id>', methods=['GET'])
+def get_planet(planets_id):
 
-    db.session.delete(favorite)
-    db.session.commit()
-    return jsonify({"success": "Favorite deleted"}), 200
+    planet = Planet.query.filter_by(id=planets_id).first()
+
+    return jsonify(planet.serialize()), 200
 
 
+#user endpoints
+
+@app.route('/users', methods=['GET'])
+def get_users():
+
+    users = User.query.all() 
+    total_users = list(map(lambda item: item.serialize(), users))
+
+    return jsonify(total_users), 200
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    body = json.loads(request.data) 
+    
+
+    query_user = User.query.filter_by(email=body["email"]).first() 
+    if query_user is None:
+        new_user = User(first_name=body["first_name"], last_name=body["last_name"], email=body["email"], password=body["password"], username=body["username"])
+        db.session.add(new_user)
+        db.session.commit()
+
+        response_body = {
+            "msg": "Created user"
+        }
+        return jsonify(response_body), 200
+
+    response_body = {
+            "msg": "existed user"
+        }
+    return jsonify(response_body), 400
+
+@app.route('/users/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+
+    user = User.query.filter_by(id=user_id).first()
+
+    return jsonify(user.serialize()), 200
+
+@app.route('/users/<int:user_id>/favorite', methods=['GET'])
+def get_favorite(user_id):
+
+    
+    favorite = Favorites.query.filter_by(user_id=user_id).all()
+    total_favorites = list(map(lambda item: item.serialize(), favorite))
+
+    return jsonify(total_favorites), 200
+
+#creating favorites planets 
+@app.route('/users/<int:user_id>/favorites/planet', methods=['POST'])
+def create_favorite_planet(user_id):
+    body = json.loads(request.data) 
+    
+    query_favorite_planet = Favorites.query.filter_by(planet_id=body["planet_id"], user_id=body["user_id"]).first() 
+    print(query_favorite_planet)
+    
+    if query_favorite_planet is None:
+        new_favorite_planet = Favorites(user_id=body["user_id"], planet_id=body["planet_id"], people_id=body["people_id"])
+        db.session.add(new_favorite_planet)
+        db.session.commit()
+
+        response_body = {
+            "msg": "Created favorite planet"
+        }
+        return jsonify(response_body), 200
+
+    response_body = {
+            "msg": "existed favorite planet"
+        }
+    return jsonify(response_body), 400
+
+
+
+@app.route('/users/<int:user_id>/favorites/people', methods=['POST'])
+def create_favorite_people(user_id):
+    body = json.loads(request.data) 
+    
+    query_favorite_people = Favorites.query.filter_by(people_id=body["people_id"], user_id=body["user_id"]).first() 
+    print(query_favorite_people)
+    
+    if query_favorite_people is None:
+        new_favorite_people = Favorites(user_id=body["user_id"], planet_id=body["planet_id"], people_id=body["people_id"])
+        db.session.add(new_favorite_people)
+        db.session.commit()
+
+        response_body = {
+            "msg": "Created favorite character"
+        }
+        return jsonify(response_body), 200
+
+    response_body = {
+            "msg": "existed favorite character"
+        }
+    return jsonify(response_body), 400
+
+@app.route('/users/<int:user_id>/favorites/planet', methods=['DELETE'])
+def delete_favorite_planet(user_id):
+    body = json.loads(request.data)
+    
+    query_favorites_planet = Favorites.query.filter_by(planet_id=body["planet_id"], user_id=user_id).first()
+    
+    if query_favorites_planet is not None:
+        db.session.delete(query_favorites_planet)
+        db.session.commit()
+
+        response_body = {
+            "msg": "Deleted favorite planet"
+        }
+        return jsonify(response_body), 200
+
+    response_body = {
+        "msg": "Favorite planet does not exist"
+    }
+    return jsonify(response_body), 400
+
+
+@app.route('/users/<int:user_id>/favorites/people', methods=['DELETE'])
+def delete_favorite_people(user_id):
+    body = json.loads(request.data)
+    query_favorite_people = Favorites.query.filter_by(people_id=body["people_id"], user_id=user_id).first()
+    
+    if query_favorite_people is not None:
+        db.session.delete(query_favorite_people)
+        db.session.commit()
+
+        response_body = {
+            "msg": "Deleted favorite people"
+        }
+        return jsonify(response_body), 200
+
+    response_body = {
+        "msg": "Favorite character does not exist"
+    }
+    return jsonify(response_body), 400
+
+
+
+
+
+
+
+
+
+ 
+
+
+# this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
--- src/models.py
+++ src/models.py
@@ -2,44 +2,86 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-# Association table for many-to-many relationship between User and People
-user_favorite_people = db.Table('user_favorite_people',
-    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
-    db.Column('people_id', db.Integer, db.ForeignKey('people.id'), primary_key=True)
-)
 
-# Association table for many-to-many relationship between User and Planet
-user_favorite_planet = db.Table('user_favorite_planet',
-    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
-    db.Column('planet_id', db.Integer, db.ForeignKey('planet.id'), primary_key=True)
-)
-
-class User(db.Model):
+class People(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-    
-    # Relationships
-    favorite_people = db.relationship('People', secondary=user_favorite_people, backref=db.backref('favorited_by', lazy='dynamic'))
-    favorite_planets = db.relationship('Planet', secondary=user_favorite_planet, backref=db.backref('favorited_by', lazy='dynamic'))
+    hair_color = db.Column(db.String(250), nullable=False)
+    eye_color = db.Column(db.String(250), nullable=False)
+    birth_year = db.Column(db.String(250), nullable=False)
+    favorites = db.relationship('Favorites', backref='people')
+
+    def __repr__(self):
+        return '<People %r>' % self.first_name
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email
+            "hair_color": self.hair_color,
+            "eye_color": self.eye_color,
+            "birth_year": self.birth_year,
+           
         }
 
-class People(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    hair_color = db.Column(db.String(50), nullable=False)
-    birth_year = db.Column(db.String(50), nullable=False)
-    eye_color = db.Column(db.String(50), nullable=False)
+
 
 class Planet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    terrain = db.Column(db.String(50), nullable=False)
-    gravity = db.Column(db.String(50), nullable=False)
-    climate = db.Column(db.String(50), nullable=False)
+    name = db.Column(db.String(250), nullable=False)
+    climate = db.Column(db.String(250), nullable=False)
+    gravity = db.Column(db.String(250), nullable=False)
+    terrain = db.Column(db.String(250), nullable=False)
+    favorites = db.relationship('Favorites', backref='planet')
+    
+    def __repr__(self):
+        return '<Planet %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "climate": self.climate,
+            "gravity": self.gravity,
+            "terrain": self.terrain,
+          
+                       
+        }
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(250), nullable=False)
+    password = db.Column(db.String(250), nullable=False)
+    favorites = db.relationship('Favorites', backref='user')
+    
+    
+    def __repr__(self):
+        return '<User %r>' % self.email
+
+    def serialize(self):
+        return {
+            "id": self.id,
+             "email": self.email,
+            "password": self.password,
+            
+            }
+
+class Favorites(db.Model):
+    __tablename__ = 'favorites'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    planet_id = db.Column(db.Integer, db.ForeignKey("planet.id"), nullable=True)
+    people_id = db.Column(db.Integer, db.ForeignKey("people.id"), nullable=True)
+    
+    def __repr__(self):
+        return '<Favorites %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "planet_id": self.planet_id,
+            "people_id": self.people_id,
+                                               
+        }
+
+#
+#     
